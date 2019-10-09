2b00b6887603bfcc71ae50ce7aeb27bf8a7b8742
Jacob See <jasee@redhat.com>
2019-10-09 16:07:13 -0500

    Add CentOS-based Dockerfile which doesn't require AnyUID, temporarily remove image version check in CLI.

--- k8s-proxy/run-priv.sh
+++ k8s-proxy/run-priv.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env sh
-set -e
+# set -e
+if ! whoami &> /dev/null; then
+  if [ -w /etc/passwd ]; then
+    echo "${USER_NAME:-telepresence}::$(id -u):0:${USER_NAME:-telepresence} user:${HOME}:/bin/bash" >> /etc/passwd
+  fi
+fi
 /usr/sbin/sshd -e
-exec env PYTHONPATH=/usr/src/app twistd --pidfile= -n -y ./forwarder.py
+exec env PYTHONPATH=/opt/app-root/src twistd --pidfile= -n -y ./forwarder.py
--- k8s-proxy/run-unpriv.sh
+++ k8s-proxy/run-unpriv.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
-set -e
+# set -e
+echo "STARTING TELEPRESENCE"
 if [ $(id -u) = 0 ]; then
     echo "root -> telepresence"
     exec su telepresence ./run-priv.sh
